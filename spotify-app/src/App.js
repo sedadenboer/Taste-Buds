9c44c0901c67375921cc2f116f7381e994b0cff7
sedadenboer <sedadenboer21@gmail.com>
2021-12-10 15:12:17 +0100

    Addition of Top Genres

--- spotify-app/src/App.js
+++ spotify-app/src/App.js
@@ -5,8 +5,8 @@ import './App.css';
 import './styles.css';
 import TopTracksResult from './TopTracks';
 import TopArtistsResult from './TopArtists';
+import TopGenresResult from './TopGenres'
 import SpotifyWebApi from 'spotify-web-api-js';
-
 
 const spotifyApi = new SpotifyWebApi();
 
@@ -29,16 +29,17 @@ function App() {
   const token = params.access_token;
   const [topTracks, setTopTracks] = useState([])
   const [topArtists, setTopArtists] = useState([])
+  const [topGenres, setTopGenres] = useState([])
 
   useEffect(() => {
     if (!token) return
     spotifyApi.setAccessToken(token)
   }, [token])
 
-  // 
+  // Get (recent) top tracks
   useEffect(() => {
-    if (!topTracks) return setTopTracks([])
     if (!token) return
+    if (!topTracks) return setTopTracks([])
 
     spotifyApi.getMyTopTracks(topTracks).then(response => {
       console.log(response.items)
@@ -53,9 +54,10 @@ function App() {
     })
   }, [topTracks, token])
 
+  // Get (recent) top artists
   useEffect(() => {
-    if (!topArtists) return setTopArtists([])
     if (!token) return
+    if (!topArtists) return setTopArtists([])
 
     spotifyApi.getMyTopArtists(topArtists).then(response => {
       console.log(response.items)
@@ -67,6 +69,41 @@ function App() {
       }))
     })
   }, [topArtists, token])
+
+  // Every artist contains a genre element
+  useEffect(() => {
+    if (!token) return
+    if (!topGenres) return setTopGenres([])
+
+    spotifyApi.getMyTopArtists(topGenres).then(response => {
+      // console.log(response.items[0].genres)
+      const genres = response.items.map(artist => {
+        return {
+          genres: artist.genres
+        }
+      })
+
+      // https://stackoverflow.com/questions/47923791/merging-json-objects-with-same-key-together
+      const completeGenresArray = genres.reduce((r, o) => {
+        Object.keys(o).forEach(k => {
+          r[k] = (r[k] || []).concat(o[k]);
+        })
+        return r
+      }, {})
+
+      // https://stackoverflow.com/questions/34396767/sort-array-by-occurrence-of-its-elements
+      const genreFrequency = completeGenresArray.genres.reduce(function (obj, val) {
+        obj[val] = (obj[val] || 0) + 1
+        return obj
+      }, {})
+
+      const sortedGenreFrequency = Object.keys(genreFrequency).sort(function (a, b) {
+        return genreFrequency[b] - genreFrequency[a]
+      })
+
+      setTopGenres(sortedGenreFrequency)
+    })
+  }, [topGenres, token])
 
   return (
     <div className="App">
@@ -83,7 +120,11 @@ function App() {
           <Tab>Top Genres</Tab>
           <Tab>Summary</Tab>
         </TabList>
+
+        {/* <p>&nbsp;</p>
+        <button>Recent</button> */}
         <p>&nbsp;</p>
+
         <TabPanel>
           <Grid container justifyContent='center' alignItems='center' spacing={10} columns={4}>
             {topTracks.map(track => (
@@ -94,6 +135,7 @@ function App() {
             ))}
           </Grid>
         </TabPanel>
+
         <TabPanel>
           <Grid container justifyContent='center' alignItems='center' spacing={10} columns={4}>
             {topArtists.map(artist => (
@@ -103,6 +145,13 @@ function App() {
               />
             ))}
           </Grid>
+        </TabPanel>
+
+        <TabPanel>
+          <p>&nbsp;</p>
+          <TopGenresResult
+            genreList={topGenres}
+          />
         </TabPanel>
       </Tabs>
     </div>
---
+++ spotify-app/src/TopGenres.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Grid } from "@material-ui/core";
+import './styles.css';
+
+export default function TopGenresResult({ genreList }) {
+    return (
+        <Grid item>
+            <div class='top-genres'>
+                <h1>1. {genreList[0]}</h1>
+                <h2>2. {genreList[1]}</h2>
+                <h2>3. {genreList[2]}</h2>
+                <h2>4. {genreList[3]}</h2>
+                <h2>5. {genreList[4]}</h2>
+            </div>
+        </Grid>
+    )
+}
